a7d16269a6d1fe520d033d53944c550b698ec8fa
DESKTOP-6N062G0\Hans <hansschumann24@gmail.com>
2018-05-07 09:06:18 -0700

    added functions for model building

--- TwitterDataCollection/helpers.py
+++ TwitterDataCollection/helpers.py
@@ -17,3 +17,27 @@ def createLag(k,allData):
     lagData['priceChange'] = lagData['price'] - lagData['lag_price']
     return lagData
 
+def create_rolling_sums(df,times):
+    """
+    Creates the training and testing sets with sums of the tweet variables
+    for given time periods as specified
+    :param df: the dataframe to use
+    :param times: an array of the times wanted 
+    :return dataframe with the created variables
+    """
+    for i in range(len(times)):
+        df[('sum' + str(times[i]) + '_count')] = df['count'].rolling(times[i]-1).sum().fillna(method = 'bfill')
+        df[('sum' + str(times[i]) + '_favorites')] = df['favorites'].rolling(times[i]-1).sum().fillna(method = 'bfill')
+        df[('sum' + str(times[i]) + '_retweets')] = df['retweets'].rolling(times[i]-1).sum().fillna(method = 'bfill')
+        df[('sum' + str(times[i]) + '_avg_sentiment')] = df['avg_sentiment'].rolling(times[i]-1).sum().fillna(method = 'bfill')
+        # subtract all but the time period of interest
+        if (i > 1):
+            df[('sum' + str(times[i]) + '_count')] = df[('sum' + str(times[i]) + '_count')] - 
+                df['count'].rolling(times[i-1]-1).sum().fillna(method = 'bfill')
+            df[('sum' + str(times[i]) + '_favorites')] = df[('sum' + str(times[i]) + '_favorites')] - 
+                df['favorites'].rolling(times[i-1]-1).sum().fillna(method = 'bfill')
+            df[('sum' + str(times[i]) + '_retweets')] = df[('sum' + str(times[i]) + '_retweets')] - 
+                df['retweets'].rolling(times[i-1]-1).sum().fillna(method = 'bfill')
+            df[('sum' + str(times[i]) + '_avg_sentiment')] = df[('sum' + str(times[i]) + '_avg_sentiment')] -
+                df['avg_sentiment'].rolling(times[i-1]-1).sum().fillna(method = 'bfill')
+    return df
